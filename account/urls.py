17e97df6353e1fcfc4d8b171502875cdc8883ebc
litvinenko636 <rabota636R>
2020-11-15 23:23:32 +0300

    commit #4

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.user_page, name='user_page'),
+    path('my_page/', views.user_page, name='user_page'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
