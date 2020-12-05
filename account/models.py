88258f22eac9b96c29316637d4daab9334105b8d
litvinenko636 <rabota636R>
2020-12-06 01:04:09 +0300

    commit #8

--- account/models.py
+++ account/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    RECRUITER = 'Рекрутер'
+    COMMON_USER = 'Пользователь'
+
+    STATUS = [
+        (RECRUITER, 'Ищу сотрудников!'),
+        (COMMON_USER, 'Ищу работу!'),
+    ]
+
+    user_role = models.CharField(
+        max_length=32,
+        choices=STATUS,
+        default='Ищу работу!',
+    )
+
+    def __str__(self):
+        return f'Профиль пользователя {self.user.username}'
