b23ea1d5fdc62ce86301122b1716a14f9e829972
litvinenko636 <rabota636R>
2020-12-05 23:59:17 +0300

    commit #7

--- account/views.py
+++ account/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, UserRegistrationForm
 
 
 @login_required
 def user_page(request):
     return render(request, 'account/user_page.html', {'section': 'user_page'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'registration/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'user_form': user_form})
