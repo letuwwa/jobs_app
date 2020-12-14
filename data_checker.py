be4fdb059f1e7a7348af5456aba59fc8e9dec7cd
litvinenko636 <rabota636R>
2020-12-15 01:34:30 +0300

    commit #24

---
+++ data_checker.py
@@ -0,0 +1,35 @@
+"""
+    DataChecker class
+"""
+
+
+class DataChecker(object):
+    def __init__(self, num_register, text_register, bool_register):
+        pass
+
+    def __text_field_check(self, some_text_data):
+        """
+            text field analytics
+        :param some_text_data:
+        :return:
+        """
+        pass
+
+    def __num_field_check(self, some_num_filed):
+        """
+            num field analytics
+        :param some_num_filed:
+        :return:
+        """
+        pass
+
+    def __bool_filed_check(self, some_bool_data):
+        """
+            bool field analytics
+        :param some_bool_data:
+        :return:
+        """
+        pass
+
+    def final_analytics(self):
+        pass
